9bb1fd888cf934980afda053c3c90f5de671ba27
lihongxi-g <lihongxi-g@users.noreply.github.com>
2026-08-14 11:49:45 +0800

    fix: statusBarsPadding import in Common.kt

--- app/src/main/java/com/safekey/authenticator/ui/components/Common.kt
+++ app/src/main/java/com/safekey/authenticator/ui/components/Common.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
