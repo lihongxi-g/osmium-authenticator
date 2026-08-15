8fd36271fd8c14ad9d862814b853022582857ba1
lihongxi-g <lihongxi-g@users.noreply.github.com>
2026-08-15 12:56:01 +0800

    fix: missing imports (fillMaxSize, CircularProgressIndicator)

--- app/src/main/java/com/safekey/authenticator/ui/components/QrCameraPreview.kt
+++ app/src/main/java/com/safekey/authenticator/ui/components/QrCameraPreview.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
