fa9f4a4d7476b492cdbb13bf5f864b5ab580c07c
lihongxi-g <lihongxi-g@users.noreply.github.com>
2026-09-13 16:34:14 +0800

    chore: versionCode 55 for the fixed v2.4.3 test build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.safekey.authenticator"
         minSdk = 26
         targetSdk = 34
-        versionCode = 54
+        versionCode = 55
         versionName = "2.4.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
